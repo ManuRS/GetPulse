2498619be214447d8b06c676e25b12fce131fbd3
Manuel Reyes <mnrs94@gmail.com>
2017-02-16 16:42:05 +0100

    Midiendo mas o meno y pide permisos en 6 o +

--- app/src/main/java/esp/uam/eps/gnb/getpulse/MainActivity.java
+++ app/src/main/java/esp/uam/eps/gnb/getpulse/MainActivity.java
@@ -1,5 +1,9 @@
 package esp.uam.eps.gnb.getpulse;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
@@ -12,6 +16,10 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 0);
+        }
 
         final Button button = (Button) findViewById(R.id.start);
         button.setOnClickListener(new View.OnClickListener() {
